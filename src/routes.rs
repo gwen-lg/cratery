47081f1174d0ae4e4a2cc764f32fef42ec9acc4e
Gwen Lg <me@gwenlg.fr>
2025-05-20 21:24:18 +0200

    wip: trace cookie send

--- src/routes.rs
+++ src/routes.rs
@@ -23,7 +23,7 @@ use axum::{BoxError, Json};
 use cookie::Key;
 use futures::future::select_all;
 use futures::{SinkExt, Stream, StreamExt};
-use log::error;
+use log::{error, trace};
 use serde::Deserialize;
 use tokio::fs::File;
 use tokio::sync::Mutex;
@@ -375,6 +375,7 @@ pub async fn api_v1_login_with_oauth_code(
         .await
         .map_err(|err| response_error(ApiError::from(err)))?;
     let cookie = auth_data.create_id_cookie(&Authentication::new_user(registry_user.id, registry_user.email.clone()));
+    trace!("api_login cookie : `{cookie}`\n\tfor user: {registry_user:?}");
     Ok((
         StatusCode::OK,
         [(SET_COOKIE, HeaderValue::from_str(&cookie.to_string()).unwrap())],

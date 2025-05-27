34440d80a2eebb275537ae1f7940becace46c17b
Gwen Lg <me@gwenlg.fr>
2025-05-27 15:46:54 +0200

    wip(error): use AuthenticationError for check_can_write

--- src/application.rs
+++ src/application.rs
@@ -937,6 +937,9 @@ pub enum AuthenticationError {
     #[error("administration is forbidden for this authentication")]
     AdministrationIsForbidden,
 
+    #[error("writing is forbidden for this authentication")]
+    WriteIsForbidden,
+
     #[error("failed to check global token")]
     GlobalToken(#[source] sqlx::Error),
 
@@ -963,7 +966,7 @@ impl AsStatusCode for AuthenticationError {
             | Self::CheckUser(_)
             | Self::CheckRoles(_) => StatusCode::INTERNAL_SERVER_ERROR,
             Self::NoUserAuthenticated => StatusCode::BAD_REQUEST,
-            Self::Forbidden | Self::AdministrationIsForbidden => StatusCode::FORBIDDEN,
+            Self::Forbidden | Self::AdministrationIsForbidden | Self::WriteIsForbidden => StatusCode::FORBIDDEN,
         }
     }
 }

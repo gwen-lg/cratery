9b084fc460de9cf303e3467b133d4e9dde0e6b08
Erieke <eweitenberg@gmail.com>
2026-07-13 23:08:39 +0200

    fix one, ignore two clippy::significant_drop_tightening lints

--- src/services/rustsec.rs
+++ src/services/rustsec.rs
@@ -43,6 +43,7 @@ struct RustSecCheckerImpl {
 impl RustSecChecker for RustSecCheckerImpl {
     /// Gets the advisories against a crate
     fn check_crate<'a>(&'a self, package: &'a str, version: &'a Version) -> FaillibleFuture<'a, Vec<SimpleAdvisory>> {
+        #[expect(clippy::significant_drop_tightening)]
         Box::pin(async move {
             let mut data = self.data.lock().await;
             data.update_data().await?;

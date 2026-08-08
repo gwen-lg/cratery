acf050292f27f0fc61d682c8eb760895f7ee95d9
Gwen Lg <me@gwenlg.fr>
2026-08-08 21:05:04 +0200

    fix: handle env var in build.rs for GIT_TAG/HASH
    this is needed to get value from Dockerfile who get it from docker-build
    workflow.

--- build.rs
+++ build.rs
@@ -2,17 +2,36 @@
  * Copyright (c) 2021 Cénotélie Opérations SAS (cenotelie.fr)
  ******************************************************************************/
 
-use std::process::Command;
+use std::{env, process::Command};
 
 fn main() {
     let db_url = "sqlite://src/empty.db";
     println!("cargo:rustc-env=DATABASE_URL={db_url}");
-    if let Ok(output) = Command::new("git").args(["rev-parse", "HEAD"]).output() {
-        let value = String::from_utf8(output.stdout).unwrap();
-        println!("cargo:rustc-env=GIT_HASH={value}");
+
+    if let Some(git_hash) = git_hash() {
+        println!("cargo:rustc-env=GIT_HASH={git_hash}");
     }
-    if let Ok(output) = Command::new("git").args(["tag", "-l", "--points-at", "HEAD"]).output() {
-        let value = String::from_utf8(output.stdout).unwrap();
-        println!("cargo:rustc-env=GIT_TAG={value}");
+    if let Some(git_tag) = git_tag() {
+        println!("cargo:rustc-env=GIT_TAG={git_tag}");
     }
+}
+
+fn git_hash() -> Option<String> {
+    env::var("GIT_HASH").ok().or_else(|| {
+        Command::new("git")
+            .args(["rev-parse", "HEAD"])
+            .output()
+            .ok()
+            .map(|output| String::from_utf8(output.stdout).unwrap_or_default().trim().to_string())
+    })
+}
+
+fn git_tag() -> Option<String> {
+    env::var("GIT_TAG").ok().or_else(|| {
+        Command::new("git")
+            .args(["tag", "-l", "--points-at", "HEAD"])
+            .output()
+            .ok()
+            .map(|output| String::from_utf8(output.stdout).unwrap_or_default().trim().to_string())
+    })
 }

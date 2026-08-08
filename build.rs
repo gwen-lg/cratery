358d675d76fa46ea539273cf9a2c448c75b009d0
Gwen Lg <me@gwenlg.fr>
2026-08-08 22:26:11 +0200

    refato env var extract in build.rs

--- build.rs
+++ build.rs
@@ -2,36 +2,38 @@
  * Copyright (c) 2021 Cénotélie Opérations SAS (cenotelie.fr)
  ******************************************************************************/
 
-use std::{env, process::Command};
+use std::{
+    env,
+    process::{Command, Output},
+};
 
 fn main() {
     let db_url = "sqlite://src/empty.db";
     println!("cargo:rustc-env=DATABASE_URL={db_url}");
 
-    if let Some(git_hash) = git_hash() {
-        println!("cargo:rustc-env=GIT_HASH={git_hash}");
-    }
-    if let Some(git_tag) = git_tag() {
-        println!("cargo:rustc-env=GIT_TAG={git_tag}");
+    extract_and_set_env("GIT_HASH", "git", &["rev-parse", "HEAD"]);
+    extract_and_set_env("GIT_TAG", "git", &["tag", "-l", "--points-at", "HEAD"]);
+}
+
+fn extract_and_set_env(env_var: &str, cmd: &str, args: &[&str]) {
+    if let Some(git_hash) = extract_from_env_or_git(env_var, cmd, args) {
+        println!("cargo:rustc-env={env_var}={git_hash}");
     }
 }
 
-fn git_hash() -> Option<String> {
-    env::var("GIT_HASH").ok().or_else(|| {
-        Command::new("git")
-            .args(["rev-parse", "HEAD"])
-            .output()
-            .ok()
-            .map(|output| String::from_utf8(output.stdout).unwrap_or_default().trim().to_string())
-    })
+fn extract_from_env_or_git(env_var: &str, cmd: &str, args: &[&str]) -> Option<String> {
+    env::var(env_var)
+        .ok()
+        .or_else(|| Command::new(cmd).args(args).output().extract_output_to_string())
 }
 
-fn git_tag() -> Option<String> {
-    env::var("GIT_TAG").ok().or_else(|| {
-        Command::new("git")
-            .args(["tag", "-l", "--points-at", "HEAD"])
-            .output()
-            .ok()
-            .map(|output| String::from_utf8(output.stdout).unwrap_or_default().trim().to_string())
-    })
+trait OptionOutputExt {
+    fn extract_output_to_string(self) -> Option<String>;
+}
+
+impl<E> OptionOutputExt for Result<Output, E> {
+    fn extract_output_to_string(self) -> Option<String> {
+        self.ok()
+            .and_then(|output| String::from_utf8(output.stdout).ok().map(|out| out.trim().to_string()))
+    }
 }

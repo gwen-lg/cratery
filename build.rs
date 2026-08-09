c15025e0bc1a84cd8deb43649d61a6eb342d20a8
Gwen Lg <me@gwenlg.fr>
2026-08-09 15:14:43 +0200

    refactor: add function to manage env var from git in build.rs

--- build.rs
+++ build.rs
@@ -7,12 +7,22 @@ use std::process::Command;
 fn main() {
     let db_url = "sqlite://src/empty.db";
     println!("cargo:rustc-env=DATABASE_URL={db_url}");
-    if let Ok(output) = Command::new("git").args(["rev-parse", "HEAD"]).output() {
-        let value = String::from_utf8(output.stdout).unwrap();
-        println!("cargo:rustc-env=GIT_HASH={value}");
+
+    extract_and_set_env("GIT_HASH", "git", &["rev-parse", "HEAD"]);
+    extract_and_set_env("GIT_TAG", "git", &["tag", "-l", "--points-at", "HEAD"]);
+}
+
+fn extract_and_set_env(env_var: &str, cmd: &str, args: &[&str]) {
+    if let Some(value) = extract_value_from_cmd(cmd, args) {
+        println!("cargo:rustc-env={env_var}={value}");
     }
-    if let Ok(output) = Command::new("git").args(["tag", "-l", "--points-at", "HEAD"]).output() {
-        let value = String::from_utf8(output.stdout).unwrap();
-        println!("cargo:rustc-env=GIT_TAG={value}");
-    }
+}
+
+/// Run a command with args and extract output in a String
+fn extract_value_from_cmd(cmd: &str, args: &[&str]) -> Option<String> {
+    Command::new(cmd)
+        .args(args)
+        .output()
+        .ok()
+        .and_then(|output| String::from_utf8(output.stdout).ok().map(|out| out.trim().to_string()))
 }
